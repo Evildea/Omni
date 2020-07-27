d77b7ec509d5d8850db7c1d4d542a60c85d73756
Daniel <danielmathewiaria@gmail.com>
2020-07-27 17:00:57 +1000

    Base Material for guudo
    created base material and instance, and substance working files

--- Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.cpp
+++ Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 #include "C:/Users/s191237/Desktop/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
+=======
+#include "C:/Users/s181674/Desktop/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
+>>>>>>> Stashed changes
