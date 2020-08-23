0f8b78efa6d68eb63f02372dea04a828f0eff3f8
Josh Freeman <joshuafreemanart@outlook.com>
2020-08-23 15:00:43 +1000

    Pivot Point Fixes, Lambert MTL removed, Openworld Greybox

--- Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.cpp
+++ Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-#include "C:/Users/s181674/Desktop/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
-=======
-<<<<<<< HEAD
-#include "C:/Users/s191215/Desktop/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
-=======
-#include "C:/Users/s191237/Desktop/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
->>>>>>> master
->>>>>>> master
+#include "C:/Users/Joshu/Desktop/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
