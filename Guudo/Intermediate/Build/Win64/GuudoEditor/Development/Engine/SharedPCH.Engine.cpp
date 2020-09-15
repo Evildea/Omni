b3fbd6a1674905c3e7d557c2efbedaff01b1ee20
Tiana <xianta@hotmail.com>
2020-09-15 12:18:53 +1000

    Modified: UE4 Scene
    Had to delete binaries to refresh files and fix compiling error

--- Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.cpp
+++ Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-#include "C:/Users/s191215/Desktop/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
-=======
-#include "C:/Users/s191281/Documents/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
->>>>>>> Development
+#include "C:/Users/s171971/Documents/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
