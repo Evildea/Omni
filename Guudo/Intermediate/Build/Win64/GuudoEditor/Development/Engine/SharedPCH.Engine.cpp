0ea25112fe20b5bcfe134c343d1b7e0e846b11e4
Mitch <mitchellbsayer@gmail.com>
2020-09-24 21:40:10 +1000

    Modified: Mitch_AdvGB Recreated Items
    When starting no silhouettes appear, when picking up an object, all silhouettes appear. When reaching customisation scene old model is imported.

--- Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.cpp
+++ Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/Daniel/Desktop/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
+#include "C:/Users/mitch/Desktop/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
