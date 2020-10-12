63a97e8deed8e39a292cc47617940df26dcff99c
Mitch <mitchellbsayer@gmail.com>
2020-10-12 18:49:49 +1100

    Modified: Project File Structure
    Deleted Open World, files were inactive/ idle. + fixed naming convention of 01_LevelOneJungle -> 02_LevelOneJungle

--- Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.cpp
+++ Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/s191215/Desktop/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
+#include "C:/Users/mitch/Desktop/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
