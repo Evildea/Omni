bc65f3df74927148647dc11c55abe00d9d612329
Tiana <xianta@hotmail.com>
2020-10-13 13:46:40 +1100

    Modified: Silhouette transparency

--- Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.cpp
+++ Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 #include "C:/Users/s181674/Desktop/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
 =======
@@ -11,3 +12,6 @@
 #include "C:/Users/s171971/Documents/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
 >>>>>>> Stashed changes
 >>>>>>> master
+=======
+#include "C:/Users/s171971/Documents/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
+>>>>>>> Stashed changes
