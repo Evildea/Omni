a56b2c5e3d436635c68e4322a89b463713d0e4eb
Josh Freeman <joshuafreemanart@outlook.com>
2020-10-14 00:12:48 +1100

    Modified: Jungle Setdressing
    Completed majority of the jungle setdressing, implement all foliage and vertex painting textures. begun environmental details and storytelling

--- Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.cpp
+++ Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/s191237/Desktop/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
+#include "C:/Users/Joshu/Desktop/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
