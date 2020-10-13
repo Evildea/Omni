69268f572eb53914846ce84dd5acda08ca2194f0
Tiana <xianta@hotmail.com>
2020-10-13 12:00:49 +1100

    Modified: UI Health bar full

--- Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.cpp
+++ Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,5 +1,9 @@
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 #include "C:/Users/s171971/Documents/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
 =======
 #include "C:/Users/s191281/Documents/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
 >>>>>>> master
+=======
+#include "C:/Users/s171971/Documents/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
+>>>>>>> Stashed changes
