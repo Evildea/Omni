f50bbd5709ab957a098ebd9178cdd5a672c9ec0b
Mitchell <mitchellbsayer@gmail.com>
2020-10-27 16:04:43 +1100

    Modified: Jungle Scene Ramp implemented

--- Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.cpp
+++ Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,13 +1,1 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-#include "C:/Users/s191281/Documents/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
-=======
-#include "C:/Users/s191215/Desktop/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
->>>>>>> master
-=======
 #include "C:/Users/s191237/Desktop/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
->>>>>>> Stashed changes
-=======
-#include "C:/Users/s171971/Documents/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
->>>>>>> Stashed changes
