b8027e387698faf187ba736b287d431bb9739be2
Daniel <danielmathewiaria@gmail.com>
2020-10-31 15:57:39 +1100

    Modified: Scan Post Process
    removed hex texture, fixed scan on pickup items

--- Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.cpp
+++ Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/Joshu/Desktop/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
+#include "C:/Users/Daniel/Desktop/Omni/Guudo/Intermediate/Build/Win64/GuudoEditor/Development/Engine/SharedPCH.Engine.h"
